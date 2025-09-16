f336439569bd6d22174e474e9b8cce996004d37e
Ryan E. <redwards@softerware.com>
2025-09-16 17:20:00 -0400

    Quartz sync: Sep 16, 2025, 5:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://safesavedocs.pages.dev/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#0d0d85ff",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
